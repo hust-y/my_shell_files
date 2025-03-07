536973e91b9f1333b1f02d79ccc03d0fa1677a5d
l-iang <3266456420@qq.com>
2025-03-07 23:07:03 +0800

    Add the debug.sh file

---
+++ debug.sh
@@ -0,0 +1,11 @@
+from="/mnt/c/Users/32664/Downloads/"
+to="/mnt/d/code/py-code/cs61a/"
+file="hw04-2cf46d1f8dc71ad7d8bacb4815de3044.zip"
+vimfile="/mnt/d/code/shell/get.sh"
+
+alias vimit="vim $vimfile"
+alias cdfrom="cd $from"
+alias cdto="cd $to"
+alias mvto="mv $from$file $to"
+alias mvfrom="mv $to$file $from"
+alias show="find . -maxdepth 1 -name $file"
